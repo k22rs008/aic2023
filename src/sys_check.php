ffa02e9eabb59c3227938abf5e6e504518afdfb4
kuhaha <kevin.yr.cheng@gmail.com>
2023-11-26 13:33:50 +0900

    Add ldap authentication

--- src/sys_check.php
+++ src/sys_check.php
@@ -1,22 +1,35 @@
 <?php
 require_once('db_config.php');
+include_once('sys_ldap.php');
 if (isset($_POST['uid'], $_POST['pass'])){
-    $u = htmlspecialchars($_POST['uid']);
-    $p = htmlspecialchars($_POST['pass']);
-    // TODO: authentication by querying KSU ldap service
-    
+    $uid = htmlspecialchars($_POST['uid']);
+    $upass = htmlspecialchars($_POST['pass']);
     // authentication by querying local database
-    $sql = "SELECT * FROM tbl_user WHERE uid= '{$u}'  AND upass='{$p}'";
+    $sql = "SELECT * FROM tbl_user WHERE uid= '{$uid}'  AND upass='{$uppass}'";
     $rs = $conn->query($sql);
     if (!$rs) die('エラー: ' . $conn->error);
     $row= $rs->fetch_assoc();
     if ($row){ //Login succeeded
-    $_SESSION['uid']   = $row['uid'];
-    $_SESSION['uname'] = $row['uname'];
-    $_SESSION['urole'] = $row['urole'];
-    header('Location:index.php');   
-    }else{
-        header('Location:?do=sys_login');   
+        $_SESSION['uid']   = $row['uid'];
+        $_SESSION['uname'] = $row['uname'];
+        $_SESSION['urole'] = $row['urole'];
+        header('Location:index.php'); 
+    }else{  // ldap authentication
+        //$ldap_info = kind_ldap($uid, $upass);
+        $ldap_info = [];
+        if ($ldap_info){  
+            /**
+             * 'uid'(ログインID)、'en_name'(英語名), 'ja_name'（漢字氏名）、
+             * 'ja_yomi'（日本語読み）、'email'(メールアドレス)、'dept'（学部・学科名)
+             * 'category'（大学教職員、学生）、'subcategory'(教育職員) 
+             *
+             * TODO: identify student/staff in database based on uid & email
+             *  For students: 
+             *      uid pattern: "k\d{2}[a-z]{2}[0-9a-z]\d{2}", 
+             *      email pattern  "@st.kyusan-u.ac.jp"
+             */
+        } 
+        header('Location:?do=sys_login');
     }
 }
 
---
+++ src/sys_ldap.php
@@ -0,0 +1,44 @@
+<?php
+function kind_ldap($userlogin, $password, $host=null){
+    if (!$host){
+        $host = "ldap1.ip.kyusan-u.ac.jp";  //LDAPサーバのホスト
+    }
+    $base = "ou=userall,dc=kyusan-u,dc=ac,dc=jp";
+
+    $dn = "uid=" . $userlogin . "," . $base;
+    $conn = ldap_connect($host);
+    if (!$conn) return false;
+    ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($conn, LDAP_OPT_REFERRALS, 0);
+    $ldap_bind = @ldap_bind($conn, $dn, $password);
+    if($ldap_bind){  //認証成功処理
+        $user = $userlogin;
+        $filter = "uid={$user}";
+        $result = ldap_search($conn, $base, $filter);
+        if (ldap_count_entries($conn, $result) > 0){
+            $entries = ldap_get_entries($conn, $result);
+            $info = $entries[0];
+            /**
+             * 'uid'(ログインID) 'mail'(メールアドレス), 
+             * 'cn'(英語名), 'jadisplayname'（漢字氏名）、'jasn'（日本語読み）
+             * 'initials'（大種別：大学教職員、学生）、'businesscategory'(教育職員)、
+             * 'carlicense'（運転免許？ 内容は学部・学科名) 
+             */
+            $record = [];
+            foreach (['uid'=>'uid',
+                'mail'=>'email',
+                'cn'=>'en_name',
+                'jadisplayname'=>'ja_name',
+                'jasn' =>'ja_yomi',
+                'initials'=>'category',
+                'businesscategory'=>'subcategory',
+                'carlicense'=>'dept',
+                ] as $key=>$item){
+                     $record[$item]= $info[$key];   
+            }
+            return $record;
+        }
+    }
+    ldap_close($conn);
+    return false;
+}
