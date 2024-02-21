787df08da45857a90054fbb0a408c60af14faf12
kuhaha <kevin.yr.cheng@gmail.com>
2024-02-21 14:01:10 +0900

    Update

--- src/models/user.php
+++ src/models/user.php
@@ -18,16 +18,41 @@ class User extends Model{
         return parent::getList($where, $orderby);
     }
 
-    function check($uid, $upass)
+    function check($userid, $passwd)
     {
         global $conn;
-        $uid = htmlspecialchars($uid);
-        $upass = htmlspecialchars($upass);
-        $sql = "SELECT * FROM tbl_user WHERE uid= '{$uid}'  AND upass='{$upass}'";
+        $userid = htmlspecialchars($userid);
+        $passwd = htmlspecialchars($passwd);
+        $sql = "SELECT * FROM tbl_user WHERE uid= '{$userid}'  AND upass='{$passwd}'";
         $rs = $conn->query($sql);
         if (!$rs) die('エラー: ' . $conn->error);
         $row = $rs->fetch_assoc();
         // if ($row) return $row; else return ldap_check()
         return $row;
+    }
+    function ldap_check($userid, $passwd)
+    {
+        $host = "ldap1.ip.kyusan-u.ac.jp";
+        $base = "ou=userall,dc=kyusan-u,dc=ac,dc=jp";
+        $dn = "uid=" . $userid . "," . $base;
+        $ldap = ldap_connect($host);
+        if(!$ldap) {echo 'connect failed!'; return false;}
+        ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);        
+        $ldap_bind = @ldap_bind($ldap, $dn, $passwd);
+        if(!$ldap_bind) {echo 'bind failed!'; return false;}
+        $target = 'k21rs034'; // ユーザ情報の取得 
+        // $target = $userid;// 本人認証 
+        $filter = "uid={$target}";
+        $result = ldap_search($ldap, $base, $filter);
+        if (ldap_count_entries($ldap, $result) == 0){
+            echo 'no entries'; return false;
+        }
+        $info = ldap_get_entries($ldap, $result);
+        /*
+        mail;
+        jao
+        */
+        return $info;        
     }
 }
