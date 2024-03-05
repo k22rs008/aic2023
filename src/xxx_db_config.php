3a744d079f22b4ead94ac4856a746ef391b58cf6
kuhaha <kevin.yr.cheng@gmail.com>
2024-03-05 21:55:52 +0900

    Add security stuffs

--- src/xxx_db_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $conn = new mysqli("localhost", "root", "", "aic_test");//＜開発時の環境設定＞
-  if ($conn->connect_errno) die($conn->connect_error);
-  $conn->set_charset('utf8'); //文字コードをutf8に設定（文字化け対策）
-?>
